e867353cd035a0d0d563e18ce6708f7cf504dba4
trumpetfish1 <trumpetfish1@gmail.com>
2025-07-17 20:06:53 -0400

    Add html

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+const coins = ["BTC", "ADA", "ETH", "WMTx", "XRP", "SNEK", "MIN", "BASE"];
 
+button = document.getElementsByName("button");
+hiddenText = document.getElementById("textContainer");
 
-const coins = ["BTC", "ADA", "ETH", "WMTx", "XRP", "SNEK", "MIN", "BASE"]
 
 function topCoin() {
-    id = Math.floor(Math.random() * coins.length)
-    //console.log(coins[id])
-    return coins[id]
+    id = Math.floor(Math.random() * coins.length);
+    //console.log(coins[id]);
+    return coins[id];
 }
 
 function howMuch() {
-   return  Math.random() * 1000
+   return  Math.random() * 1000;
 }
 
 
 function iDeclare(amount, coin)  {
-    return `I am going to purchase $${amount} of ${coin}!!!`
+    return `I am going to purchase $${amount} in ${coin}!!!`;
 }
 
-myCoin = topCoin();
-amountCoin = howMuch()
-sentence = iDeclare(amountCoin, myCoin)
+function handleClick() {
+    myCoin = topCoin();
+    amountCoin = howMuch();
+    sentence = iDeclare(amountCoin, myCoin);
+    hiddenText.style.display="block";
+    console.log(sentence);
 
-console.log(sentence)
+
+
+
+
+button.addEventListener("click", handleClick);
+
+
