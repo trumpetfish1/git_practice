f9ef72f08bc8f2e201f5768c12e7d1dd79fb06c1
trumpetfish1 <trumpetfish1@gmail.com>
2025-07-17 20:21:15 -0400

    Cleanup

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const coins = ["BTC", "ADA", "ETH", "WMTx", "XRP", "SNEK", "MIN", "BASE"];
 
-button = document.getElementsById("button");
-hiddenText = document.getElementById("textContainer");
+const button = document.getElementById("button");
+const hiddenText = document.getElementById("textContainer");
 
 
 function topCoin() {
-    id = Math.floor(Math.random() * coins.length);
+    let id = Math.floor(Math.random() * coins.length);
     //console.log(coins[id]);
     return coins[id];
 }
 
 function howMuch() {
-   return  Math.random() * 1000;
+   return  (Math.random() * 1000).toFixed(2);
 }
 
 function iDeclare(amount, coin)  {
@@ -19,11 +19,11 @@ function iDeclare(amount, coin)  {
 }
 
 function handleClick() {
-    myCoin = topCoin();
-    amountCoin = howMuch();
-    sentence = iDeclare(amountCoin, myCoin);
-    hiddenText.style.display="block";
-    console.log(sentence);
+    const myCoin = topCoin();
+    const amountCoin = howMuch();
+    const sentence = iDeclare(amountCoin, myCoin);
+    hiddenText.innerHTML = `<p>${sentence}</p>`;  // Display the fortune text
+    hiddenText.style.display = "block";
 
 }
 
